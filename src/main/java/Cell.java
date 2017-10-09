f5eb4eab8057ce9a5ed48edc8c3f5e5091b8d663
Timur <timur.salakhetdinov@gmail.com>
2017-10-09 03:54:12 +0300

    add cell is alive in next generation test

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
     private final boolean alive;
+    private List<Cell> neighbours = new ArrayList<>();
+    private int numOfAliveNeighbours = 0;
 
     public Cell(boolean isAlive) {
         this.alive = isAlive;
     }
 
     public Cell nextGeneration() {
+        if (numOfAliveNeighbours == 2 || numOfAliveNeighbours == 3)
+            return new Cell(true);
         return new Cell(false);
     }
 
     public boolean isAlive() {
         return alive;
+    }
+
+    public void addNeighbour(Cell cell) {
+        if (cell.isAlive())
+            numOfAliveNeighbours++;
+        neighbours.add(cell);
     }
 }
--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,11 +1,20 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CellTest {
     @Test
     public void underPopulation() throws Exception {
         Cell cell = new Cell(true);
         assertFalse(cell.nextGeneration().isAlive());
+    }
+
+    @Test
+    public void stayAlive() throws Exception {
+        Cell cell = new Cell(true);
+        cell.addNeighbour(new Cell(true));
+        cell.addNeighbour(new Cell(true));
+        assertTrue(cell.nextGeneration().isAlive());
     }
 }
