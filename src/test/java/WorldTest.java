43151faea1c4212e31d3e5870823b8dbf8c6b48e
Timur <timur.salakhetdinov@gmail.com>
2017-10-10 00:15:39 +0300

    clean tests

--- src/test/java/WorldTest.java
+++ src/test/java/WorldTest.java
@@ -4,22 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 public class WorldTest {
     @Test
-    public void printOneCellWorld() throws Exception {
-        String input = "*";
-        World world = new World(input);
-        assertEquals(input, world.toString());
-    }
-
-    @Test
-    public void horizontalCellWorldToString() throws Exception {
-        String input = "*.**..**";
-        World world = new World(input);
-        assertEquals(input, world.toString());
-    }
-
-    @Test
-    public void verticalCellWorldToString() throws Exception {
-        String input = "*\n.\n.\n*";
+    public void rectangleWorldToString() throws Exception {
+        String input = "*..\n.**\n..*";
         World world = new World(input);
         assertEquals(input, world.toString());
     }
