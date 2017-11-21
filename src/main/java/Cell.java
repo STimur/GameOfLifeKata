4f5a8f5d44db983184113013ebf6539328f65654
Timur <timur.salakhetdinov@gmail.com>
2017-11-21 03:47:16 +0300

    clean code

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -10,14 +10,37 @@ public class Cell {
     }
 
     public Cell nextGen() {
-        if (!isAlive) {
-            if (numOfAliveNeighbours == 3)
-                return new Cell('*');
-            return new Cell('.');
-        }
-        if (numOfAliveNeighbours < 2 || numOfAliveNeighbours > 3)
+        if (!isAlive)
+            return nextGenForDeadCell();
+        return nextGenForAliveCell();
+    }
+
+    private Cell nextGenForAliveCell() {
+        if (shouldDie())
             return new Cell('.');
         return new Cell('*');
+    }
+
+    private Cell nextGenForDeadCell() {
+        if (shouldRevive())
+            return new Cell('*');
+        return new Cell('.');
+    }
+
+    private boolean shouldDie() {
+        return isUnderPopulated() || isOverPopulated();
+    }
+
+    private boolean isOverPopulated() {
+        return numOfAliveNeighbours > 3;
+    }
+
+    private boolean isUnderPopulated() {
+        return numOfAliveNeighbours < 2;
+    }
+
+    private boolean shouldRevive() {
+        return numOfAliveNeighbours == 3;
     }
 
     public void addNeighbour(Cell cell) {
