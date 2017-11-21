2994a251a5f6fcb92f4aac1ff79dc655e7f9b609
Timur <timur.salakhetdinov@gmail.com>
2017-11-21 03:08:34 +0300

    add test for dead cell with three alive neighbours

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,12 +1,25 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Cell {
+    public final boolean isAlive;
+    private int numOfAliveNeighbours;
+
     public Cell(char c) {
+        if (c == '.')
+            isAlive = false;
+        else if (c == '*')
+            isAlive = true;
+        else
+            throw new NotImplementedException();
     }
 
     public Cell nextGen() {
+        if (numOfAliveNeighbours == 3)
+            return new Cell('*');
         return new Cell('.');
     }
 
-    public boolean isAlive() {
-        return false;
+    public void addNeighbour(Cell cell) {
+        numOfAliveNeighbours++;
     }
 }
--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CellTest {
     @Test
     public void deadCellWithNoNeighboursStaysDead() throws Exception {
         Cell cell = new Cell('.');
-        assertFalse(cell.nextGen().isAlive());
+        assertFalse(cell.nextGen().isAlive);
+    }
+
+    @Test
+    public void deadCellWithThreeAliveNeighboursBecomesAlive() throws Exception {
+        Cell cell = new Cell('.');
+        cell.addNeighbour(new Cell('*'));
+        cell.addNeighbour(new Cell('*'));
+        cell.addNeighbour(new Cell('*'));
+        assertTrue(cell.nextGen().isAlive);
     }
 }
