7a1b0c706920a50f7b8bb7cdb03d8ede3298410e
Timur <timur.salakhetdinov@gmail.com>
2017-11-21 05:11:15 +0300

    add console app showing blinker oscillator in action

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        Field field = new Field(".*.\n.*.\n.*.");
+        int i = 0;
+        while (true) {
+            System.out.println("Generation " + i + ":");
+            System.out.println(field);
+            System.out.println();
+            Thread.sleep(1000);
+            field = field.nextGen();
+            i++;
+        }
+    }
+}
