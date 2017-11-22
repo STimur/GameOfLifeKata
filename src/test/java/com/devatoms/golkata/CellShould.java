a0158446bdabb6314ab008bf906830da6f0d5c35
Timur <timur.salakhetdinov@gmail.com>
2017-11-23 01:38:18 +0300

    clean tests

--- src/test/java/com/devatoms/golkata/CellShould.java
+++ src/test/java/com/devatoms/golkata/CellShould.java
@@ -1,48 +1,39 @@
 package com.devatoms.golkata;
 
-import org.junit.Before;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class CellShould {
 
     private Cell cell;
 
-    private Cell createDeadCell() {
+    private Cell createCell(boolean isAlive) {
+        if (isAlive)
+            return new Cell('*');
         return new Cell('.');
-    }
-
-    private Cell createAliveCell() {
-        return new Cell('*');
     }
 
     private void addAliveNeighbours(int numOfNeighbours) {
         for (int i = 0; i < numOfNeighbours; i++)
-            cell.addNeighbour(createAliveCell());
+            cell.addNeighbour(createCell(true));
     }
 
     @Test
+    @Parameters({
+            "false, 3, true",
+            "false, 0, false",
+            "true, 2, true"
+    })
     public void
-    revive_if_has_three_alive_neighbours() {
-        cell = createDeadCell();
-        addAliveNeighbours(3);
-        assertThat(cell.nextGen().isAlive, is(true));
-    }
-
-    @Test
-    public void
-    stay_dead_if_has_no_neighbours() {
-        cell = createDeadCell();
-        assertThat(cell.nextGen().isAlive, is(false));
-    }
-
-    @Test
-    public void
-    stay_alive_if_has_two_alive_neighbours() {
-        cell = createAliveCell();
-        addAliveNeighbours(2);
-        assertThat(cell.nextGen().isAlive, is(true));
+    create_next_generation(boolean isAlive, int numOfAliveNeighbours, boolean isAliveInNextGeneration) {
+        cell = createCell(isAlive);
+        addAliveNeighbours(numOfAliveNeighbours);
+        assertThat(cell.nextGen().isAlive, is(isAliveInNextGeneration));
     }
 }
